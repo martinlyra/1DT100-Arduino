d103069761324145b8c2afd4682a183cd9449e2c
martinlyra <martinlyra@users.noreply.github.com>
2016-09-28 11:19:05 +0200

    Add files via upload

---
+++ lab-4-2016-09-28/lab_4_5.ino
@@ -0,0 +1,22 @@
+void setup()
+{
+  pinMode(13,OUTPUT);
+  Serial.begin(9600);
+}
+
+char val;
+
+void loop() { 
+  if( Serial.available() )       // if data is available to read
+  {
+    val = Serial.read();         // read it and store it in 'val'
+  }
+  if( val == 'H' )               // if 'H' was received
+  {
+    digitalWrite(13, HIGH);  // turn ON the LED
+  } else { 
+    digitalWrite(13, LOW);   // otherwise turn it OFF
+  }
+  delay(100);                    // wait 100ms for next reading
+} 
+
