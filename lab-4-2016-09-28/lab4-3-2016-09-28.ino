e841a54d6e33676fd3caf12f90d839add93af969
martinlyra <martinlyra@users.noreply.github.com>
2016-09-30 14:58:14 +0200

    Add files via upload

---
+++ lab-4-2016-09-28/lab4-3-2016-09-28.ino
@@ -0,0 +1,27 @@
+float valueX, valueY;
+int restX, restY;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  restX = analogRead(0);
+  restY = analogRead(1);
+}
+
+void loop() {
+  int val0 = analogRead(0);
+  int val1 = analogRead(1);
+
+  valueX = (val0 - restX)/67.584;
+  valueY = (val1 - restY)/67.584;
+
+  Serial.print("X:");
+  Serial.print(valueX);
+  Serial.print(";"); // splitter character for indexOf() in Processing
+  Serial.print("Y:");
+  Serial.print(valueY);
+
+  Serial.println("");
+
+  delay(30);
+}
---
+++ lab-4-2016-09-28/lab4-4-2016-09-28.ino
@@ -0,0 +1,33 @@
+float valueX, valueY;
+int restX, restY;
+
+int buttonState;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  restX = analogRead(0);
+  restY = analogRead(1);
+}
+
+void loop() {
+  int val0 = analogRead(0);
+  int val1 = analogRead(1);
+
+  buttonState = analogRead(5) == 0 ? 1 : 0;
+  
+  valueX = (val0 - restX)/67.584;
+  valueY = (val1 - restY)/67.584;
+
+  Serial.print("B");
+  Serial.print(buttonState);
+  Serial.print("X:");
+  Serial.print(valueX);
+  Serial.print(";"); // splitter character for indexOf() in Processing
+  Serial.print("Y:");
+  Serial.print(valueY);
+
+  Serial.println("");
+
+  delay(30);
+}
