cd2b880db704ac3777bea3d85ed10b1d8d01a53e
martinlyra <martinlyra@users.noreply.github.com>
2016-09-30 15:08:54 +0200

    Commented lab4-3-2016-09-28.ino

--- lab-4-2016-09-28/lab4-3-2016-09-28.ino
+++ lab-4-2016-09-28/lab4-3-2016-09-28.ino
@@ -1,27 +1,33 @@
-float valueX, valueY;
-int restX, restY;
+float valueX, valueY;       // variables where we keep our finished values
+int restX, restY;           // variables where we keep our cabliration of the accelerometer
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  restX = analogRead(0);
-  restY = analogRead(1);
+  Serial.begin(9600);       //setup the serial connection
+  //cabilrate the accelerometer
+  restX = analogRead(0);    // x-axis
+  restY = analogRead(1);    // y-axis
 }
 
 void loop() {
-  int val0 = analogRead(0);
-  int val1 = analogRead(1);
+  // read the values from the accelerometer
+  int val0 = analogRead(0); // x-axis
+  int val1 = analogRead(1); // y-axis
 
-  valueX = (val0 - restX)/67.584;
-  valueY = (val1 - restY)/67.584;
+  // get an factor by subtracting the rest value from the read value and a constant factor
+  valueX = (val0 - restX)/67.584; // X
+  valueY = (val1 - restY)/67.584; // Y
 
+  // Time to output it!
   Serial.print("X:");
   Serial.print(valueX);
   Serial.print(";"); // splitter character for indexOf() in Processing
   Serial.print("Y:");
   Serial.print(valueY);
+  // The lines above will print a string in this format:
+  // X:[valueX];Y:[valueY]
+  // Where the square brackets are where the variables will be placed, and as stated, which.
+  
+  Serial.println(""); // We are finished, terminate the string by sending a '\n'
 
-  Serial.println("");
-
-  delay(30);
+  delay(30); // We don't need to send tons of data to the other end of the serial connection. (Wait 30 ms)
 }
